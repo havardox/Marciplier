03cdd8710ac75ed36405d8db5ec14744325ac4ae
havardox <harald.varner@gmail.com>
2024-11-25 19:00:11 +0200

    Improve archive extraction util

--- marciplier/example.py
+++ marciplier/example.py
@@ -5,12 +5,12 @@ from marciplier.marc_record import MarcRecord
 from marciplier.utils import download_file, extract_archive
 
 download_file(url="https://data.digar.ee/erb/ERB_perioodika.zip", filename="ERB_perioodika.zip", folder="data")
-src = extract_archive(archive_path="data/ERB_perioodika.zip", extract_to="data")
+src = extract_archive(archive_path="data/ERB_perioodika.zip", extract_to="data")[0]
 
 xml_to_json_result = convert(src, src_format="xml", target_format="json")
 
 with open ("data/ERB_perioodika1.json", "w") as f:
-    json.dump(obj=xml_to_json_result, fp=f, indent=2)
+    json.dump(obj=xml_to_json_result, fp=f)
 
 xml_to_records_result: list[MarcRecord] = convert(src, src_format="xml", target_format="records")
 for record in xml_to_records_result[:10]:
--- marciplier/utils.py
+++ marciplier/utils.py
@@ -8,9 +8,17 @@ import py7zr
 import requests
 
 
-def prettify_xml(xml_str, indent: str = "\t"):
-    """Return a pretty-printed XML string for the Element."""
+def prettify_xml(xml_str: str, indent: str = "\t") -> str:
+    """
+    Returns a pretty-printed XML string.
 
+    Args:
+        xml_str: A string containing the XML to be formatted.
+        indent: A string to use for indentation. Defaults to a tab character.
+
+    Returns:
+        The formatted XML string.
+    """
     reparsed = minidom.parseString(xml_str)
     return reparsed.toprettyxml(indent=indent)
 
@@ -18,18 +26,37 @@ def prettify_xml(xml_str, indent: str = "\t"):
 def download_file(
     url: str, folder: os.PathLike | str, filename: str | None = None, **requests_kwargs
 ) -> Path:
+    """
+    Downloads a file from the given URL and saves it to the specified folder.
+
+    Args:
+        url: The URL of the file to download.
+        folder: The directory where the downloaded file should be saved.
+        filename: Optional; the name to save the file as. If not provided, attempts to derive the
+                  filename from the `Content-Disposition` header.
+        **requests_kwargs: Additional keyword arguments to pass to the `requests.get` method.
+
+    Returns:
+        The path to the downloaded file.
+
+    Raises:
+        ValueError: If the filename cannot be inferred from the response headers and is not provided.
+        requests.exceptions.RequestException: For HTTP errors.
+    """
     download_path = Path(folder)
     download_path.mkdir(parents=True, exist_ok=True)
 
     response = requests.get(url, **requests_kwargs)
     response.raise_for_status()
+
     if filename is None:
-        content_dispostion = response.headers.get("content-disposition")
-        if content_dispostion is None:
+        content_disposition = response.headers.get("content-disposition")
+        if content_disposition is None:
             raise ValueError(
-                "Unable to determine filename because no Content-Dispositon header was found for the request performed on the URL. Please provide the download_filename argument."
+                "Unable to determine filename because no Content-Disposition header was found. "
+                "Please provide the filename argument."
             )
-        filename = content_dispostion.split("=", -1)[-1]
+        filename = content_disposition.split("=", -1)[-1]
 
     download_path = download_path / filename
 
@@ -39,24 +66,56 @@ def download_file(
 
 
 def extract_archive(
-    archive_path: os.PathLike | str, extract_to: os.PathLike | str
-) -> Path:
+    archive_path: os.PathLike | str,
+    extract_to: os.PathLike | str,
+    targets_to_extract: list[str] = [],
+) -> list[Path]:
+    """
+    Extracts an archive file (zip, tar, or 7z) to a specified directory.
+
+    Args:
+        archive_path: The path to the archive file.
+        extract_to: The directory where the contents of the archive should be extracted.
+        targets_to_extract: Optional; a list of specific file names to extract. If empty,
+                            extracts all files. Defaults to an empty list.
+
+    Returns:
+        A list of paths to the extracted files.
+
+    Raises:
+        ValueError: If the archive format is unsupported.
+    """
     archive_path = Path(archive_path)
     extract_to = Path(extract_to)
 
     if archive_path.suffix == ".zip":
         with zipfile.ZipFile(archive_path, "r") as archive:
-            archive.extractall(extract_to)
-            return extract_to / archive.namelist()[0]
+            if targets_to_extract:
+                for target in targets_to_extract:
+                    archive.extract(target, extract_to)
+                return [extract_to / target for target in targets_to_extract]
+            else:
+                archive.extractall(extract_to)
+                return [extract_to / name for name in archive.namelist()]
+
     elif archive_path.suffix in {".tar", ".tar.gz", ".tgz"}:
         with tarfile.open(archive_path, "r:*") as archive:
-            archive.extractall(extract_to)
-            return extract_to / archive.getnames()[0]
+            if targets_to_extract:
+                for target in targets_to_extract:
+                    archive.extract(target, extract_to)
+                return [extract_to / target for target in targets_to_extract]
+            else:
+                archive.extractall(extract_to)
+                return [extract_to / name for name in archive.getnames()]
+
     elif archive_path.suffix == ".7z":
         with py7zr.SevenZipFile(archive_path, "r") as archive:
-            archive.extractall(path=extract_to)
-            return extract_to / archive.getnames()[0]
+            if targets_to_extract:
+                archive.extract(extract_to, targets_to_extract)
+                return [extract_to / target for target in targets_to_extract]
+            else:
+                archive.extractall(extract_to)
+                return [extract_to / name for name in archive.getnames()]
+
     else:
         raise ValueError(f"Unsupported archive format: {archive_path.suffix}")
-    
-
